b8fdcab97a31e169b24a0a73c0dd4c1b5cf240db
RKmax00 <rohankoli0577@gmail.com>
2026-05-14 19:46:41 +0530

    Update owob.py

--- owob.py
+++ owob.py
@@ -3,26 +3,37 @@ import time
 
 app = Flask(__name__)
 
+# Your GIF links
 images = [
-    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449531834794115/beyond_the_other_side.gif?ex=6a07074f&is=6a05b5cf&hm=a0e27646053a4271b04cebbbae40cab94d81cc724234c96c5b180f6eab7cc571&",
-
-    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449532388311090/Not_sure_where_this_is_from_but_I_find_it_calming_and_pleasing_to_look_at___.gif?ex=6a07074f&is=6a05b5cf&hm=0b35a1d1000167f7051d249cc60ad2c30c2ca0189cd4bf54a222c4af0f4a4176&",
-
-    "https://media.discordapp.net/attachments/1469338751296733378/1504449532975648859/09b902d11b569df1225ad2e8a91572e3_gif_500279.gif?ex=6a07074f&is=6a05b5cf&hm=e9b8532eaa813e3ea5a00e9f0719327bf685ded9ca92aadd94e51bb526af0682&=&width=625&height=349",
-
-    "https://media.discordapp.net/attachments/1469338751296733378/1504449533466251294/Gif_aesthetic_anime.gif?ex=6a070750&is=6a05b5d0&hm=0d7a5b8b4bfa7c21a456f621d1d4592ccf30b5367d6b6322d42ab4b8b7f16ae2&=&width=675&height=380",
+    "https://media.discordapp.net/attachments/1469338751296733378/1504449534896640060/download.gif?ex=6a070750&is=6a05b5d0&hm=ed5e12987e0917f3dc29dee61359405e2bc47da8a9575e7f8b7b7957fb704dbd&=&width=625&height=465",
 
     "https://media.discordapp.net/attachments/1469338751296733378/1504449534036541521/posted_by_ernoticon_on_Tumblr.gif?ex=6a070750&is=6a05b5d0&hm=f2fa37a9f3fc7a4be76203d93d5d68b29c70e00e9004f7f5495ff8255ff43269&=&width=625&height=345",
 
-    "https://media.discordapp.net/attachments/1469338751296733378/1504449534896640060/download.gif?ex=6a070750&is=6a05b5d0&hm=ed5e12987e0917f3dc29dee61359405e2bc47da8a9575e7f8b7b7957fb704dbd&=&width=625&height=465"
+    "https://media.discordapp.net/attachments/1469338751296733378/1504449533466251294/Gif_aesthetic_anime.gif?ex=6a070750&is=6a05b5d0&hm=0d7a5b8b4bfa7c21a456f621d1d4592ccf30b5367d6b6322d42ab4b8b7f16ae2&=&width=675&height=380",
+
+    "https://media.discordapp.net/attachments/1469338751296733378/1504449532975648859/09b902d11b569df1225ad2e8a91572e3_gif_500279.gif?ex=6a07074f&is=6a05b5cf&hm=e9b8532eaa813e3ea5a00e9f0719327bf685ded9ca92aadd94e51bb526af0682&=&width=625&height=349",
+
+    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449532388311090/Not_sure_where_this_is_from_but_I_find_it_calming_and_pleasing_to_look_at___.gif?ex=6a07074f&is=6a05b5cf&hm=0b35a1d1000167f7051d249cc60ad2c30c2ca0189cd4bf54a222c4af0f4a4176&",
+
+    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449531834794115/beyond_the_other_side.gif?ex=6a07074f&is=6a05b5cf&hm=a0e27646053a4271b04cebbbae40cab94d81cc724234c96c5b180f6eab7cc571&"
 ]
 
 @app.route("/")
-def rotate():
+def rotate_gif():
 
-    current = int(time.time() // 14) % len(images)
+    # Change GIF every 14 seconds globally
+    current_index = int(time.time() / 14) % len(images)
 
-    return redirect(images[current], code=302)
+    # Current GIF
+    current_gif = images[current_index]
+
+    # Add changing timestamp to bypass cache
+    separator = "&" if "?" in current_gif else "?"
+
+    return redirect(
+        f"{current_gif}{separator}t={int(time.time())}",
+        code=302
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

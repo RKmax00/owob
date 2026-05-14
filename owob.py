b731e9065139937eea0b83d918a8fc2e591af842
RKmax00 <rohankoli0577@gmail.com>
2026-05-14 17:34:02 +0530

    Update owob.py

--- owob.py
+++ owob.py
@@ -1,33 +1,20 @@
-from flask import Flask, Response
-import requests
+from flask import Flask, redirect
 import itertools
 
 app = Flask(__name__)
 
 images = itertools.cycle([
-    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449534896640060/download.gif",
-    
-    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449534036541521/posted_by_ernoticon_on_Tumblr.gif",
-    
-    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449533466251294/Gif_aesthetic_anime.gif",
-    
-    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449532975648859/09b902d11b569df1225ad2e8a91572e3_gif_500279.gif",
-    
-    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449532388311090/Not_sure_where_this_is_from_but_I_find_it_calming_and_pleasing_to_look_at___.gif",
-    
-    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449531834794115/beyond_the_other_side.gif"
+    "https://files.catbox.moe/c0asdk.gif",
+    "https://files.catbox.moe/85wmoy.gif",
+    "https://files.catbox.moe/348isi.gif",
+    "https://files.catbox.moe/looo1r.gif",
+    "https://files.catbox.moe/ac041w.gif",
+    "https://files.catbox.moe/ux1syg.gif"
 ])
 
 @app.route("/")
-def rotate_image():
-    url = next(images)
-
-    r = requests.get(url)
-
-    return Response(
-        r.content,
-        content_type=r.headers["Content-Type"]
-    )
+def rotate():
+    return redirect(next(images), code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

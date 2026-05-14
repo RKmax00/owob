277c5437601af09bf7ccbbf4d8594af485f279dd
RKmax00 <rohankoli0577@gmail.com>
2026-05-14 20:08:42 +0530

    Update owob.py

--- owob.py
+++ owob.py
@@ -1,36 +1,28 @@
-from flask import Flask, Response
-import requests
+from flask import Flask, redirect
 import time
 
 app = Flask(__name__)
 
 images = [
-    "https://media.discordapp.net/attachments/1469338751296733378/1504449534896640060/download.gif?ex=6a070750&is=6a05b5d0&hm=ed5e12987e0917f3dc29dee61359405e2bc47da8a9575e7f8b7b7957fb704dbd&=&width=625&height=465",
+    "https://static.wikia.nocookie.net/owobot/images/4/42/Great_Sword.png/revision/latest?cb=20191128112429",
 
-    "https://media.discordapp.net/attachments/1469338751296733378/1504449534036541521/posted_by_ernoticon_on_Tumblr.gif?ex=6a070750&is=6a05b5d0&hm=f2fa37a9f3fc7a4be76203d93d5d68b29c70e00e9004f7f5495ff8255ff43269&=&width=625&height=345",
-
-    "https://media.discordapp.net/attachments/1469338751296733378/1504449533466251294/Gif_aesthetic_anime.gif?ex=6a070750&is=6a05b5d0&hm=0d7a5b8b4bfa7c21a456f621d1d4592ccf30b5367d6b6322d42ab4b8b7f16ae2&=&width=675&height=380",
-
-    "https://media.discordapp.net/attachments/1469338751296733378/1504449532975648859/09b902d11b569df1225ad2e8a91572e3_gif_500279.gif?ex=6a07074f&is=6a05b5cf&hm=e9b8532eaa813e3ea5a00e9f0719327bf685ded9ca92aadd94e51bb526af0682&=&width=625&height=349"
+    "https://static.wikia.nocookie.net/owobot/images/7/77/Defender%27s_Aegis.png/revision/latest?cb=20191128112427"
 ]
 
 @app.route("/")
-def gif():
+def rotate():
 
+    # Change image every 14 seconds
     current = int(time.time() // 14) % len(images)
 
-    gif_url = images[current]
+    current_image = images[current]
 
-    response = requests.get(gif_url)
+    # Add timestamp to help bypass cache
+    separator = "&" if "?" in current_image else "?"
 
-    return Response(
-        response.content,
-        mimetype="image/gif",
-        headers={
-            "Cache-Control": "no-cache, no-store, must-revalidate",
-            "Pragma": "no-cache",
-            "Expires": "0"
-        }
+    return redirect(
+        f"{current_image}{separator}t={int(time.time())}",
+        code=302
     )
 
 if __name__ == "__main__":

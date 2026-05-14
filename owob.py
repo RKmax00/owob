9f0e6700f0b05d5e64244fa6fb7743b427005a90
RKmax00 <rohankoli0577@gmail.com>
2026-05-14 21:57:09 +0530

    Update owob.py

--- owob.py
+++ owob.py
@@ -1,43 +1,29 @@
-from flask import Flask, send_from_directory
-import threading
-import time
-import shutil
+from flask import Flask, redirect
+import random
 
 app = Flask(__name__)
 
-gifs = [
-    "static/1.gif",
-    "static/2.gif",
-    "static/3.gif",
-    "static/4.gif"
+images = [
+
+    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449531834794115/beyond_the_other_side.gif?ex=6a07074f&is=6a05b5cf&hm=a0e27646053a4271b04cebbbae40cab94d81cc724234c96c5b180f6eab7cc571&",
+
+    "https://cdn.discordapp.com/attachments/1469338751296733378/1504449532388311090/Not_sure_where_this_is_from_but_I_find_it_calming_and_pleasing_to_look_at___.gif?ex=6a07074f&is=6a05b5cf&hm=0b35a1d1000167f7051d249cc60ad2c30c2ca0189cd4bf54a222c4af0f4a4176&",
+
+    "https://media.discordapp.net/attachments/1469338751296733378/1504449532975648859/09b902d11b569df1225ad2e8a91572e3_gif_500279.gif?ex=6a07074f&is=6a05b5cf&hm=e9b8532eaa813e3ea5a00e9f0719327bf685ded9ca92aadd94e51bb526af0682&=&width=625&height=349",
+
+    "https://media.discordapp.net/attachments/1469338751296733378/1504449533466251294/Gif_aesthetic_anime.gif?ex=6a070750&is=6a05b5d0&hm=0d7a5b8b4bfa7c21a456f621d1d4592ccf30b5367d6b6322d42ab4b8b7f16ae2&=&width=675&height=380",
+
+    "https://media.discordapp.net/attachments/1469338751296733378/1504449534036541521/posted_by_ernoticon_on_Tumblr.gif?ex=6a070750&is=6a05b5d0&hm=f2fa37a9f3fc7a4be76203d93d5d68b29c70e00e9004f7f5495ff8255ff43269&=&width=625&height=345",
+
+    "https://media.discordapp.net/attachments/1469338751296733378/1504449534896640060/download.gif?ex=6a070750&is=6a05b5d0&hm=ed5e12987e0917f3dc29dee61359405e2bc47da8a9575e7f8b7b7957fb704dbd&=&width=625&height=465"
 ]
 
-current = 0
-
-def rotate_gifs():
-    global current
-
-    while True:
-        shutil.copyfile(
-            gifs[current],
-            "static/current.gif"
-        )
-
-        current = (current + 1) % len(gifs)
-
-        time.sleep(14)
-
-threading.Thread(
-    target=rotate_gifs,
-    daemon=True
-).start()
-
 @app.route("/")
-def serve_gif():
-    return send_from_directory(
-        "static",
-        "current.gif"
-    )
+def rotate():
+
+    image = random.choice(images)
+
+    return redirect(image, code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

b050b3b1c561561dd917ec86d97f7b5be0c0c653
RKmax00 <rohankoli0577@gmail.com>
2026-05-14 17:56:44 +0530

    Update owob.py

--- owob.py
+++ owob.py
@@ -1,27 +1,28 @@
-from flask import Flask, redirect
+from flask import Flask, send_file
+import itertools
+import os
 
 app = Flask(__name__)
 
-images = [
-    "https://files.catbox.moe/c0asdk.gif",
-    "https://files.catbox.moe/85wmoy.gif",
-    "https://files.catbox.moe/348isi.gif",
-    "https://files.catbox.moe/looo1r.gif",
-    "https://files.catbox.moe/ac041w.gif",
-    "https://files.catbox.moe/ux1syg.gif"
-]
+gif_cycle = itertools.cycle([
+    "1.gif",
+    "2.gif",
+    "3.gif",
+    "4.gif",
+    "5.gif",
+    "6.gif",
+])
 
-counter = 0
+@app.route("/battle.gif")
+def battle_gif():
+    gif = next(gif_cycle)
+    path = os.path.join("gifs", gif)
 
-@app.route("/")
-def rotate():
-    global counter
-
-    url = images[counter]
-
-    counter = (counter + 1) % len(images)
-
-    return redirect(url)
+    return send_file(
+        path,
+        mimetype="image/gif",
+        cache_timeout=0
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
